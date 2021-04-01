9f06cb3b5b889d371eadf3fef1c74e73557f5544
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-04-01 13:34:41 +0600

    single product shown

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const cors = require("cors");
 require("dotenv").config();
+const ObjectId = require('mongodb').ObjectId
 const port = 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.win4w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -34,6 +35,14 @@ client.connect((err) => {
             console.log(result.insertedCount);
             res.send(result.insertedCount > 0);
         });
+    });
+
+    app.get("/product/:_id", (req, res) => {
+        productCollection
+            .find({ _id: ObjectId(req.params._id) })
+            .toArray((err, documents) => {
+                res.send(documents);
+            });
     });
     
 });
