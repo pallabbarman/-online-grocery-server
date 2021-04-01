8405816ba1940daf0a36cdfac9f51873a5bf5815
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-04-01 22:08:18 +0600

    port added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const MongoClient = require("mongodb").MongoClient;
 const cors = require("cors");
 require("dotenv").config();
 const ObjectId = require("mongodb").ObjectId;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.win4w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
